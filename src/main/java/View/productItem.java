f80a8ab8f0d14f09a4ec1893ee54a1b052266f7e
karimelkazaz <es-KarimAshraf2025@alexu.edu.eg>
2022-12-15 18:48:49 +0200

    Leeesgoo

---
+++ src/main/java/View/productItem.java
@@ -0,0 +1,32 @@
+package View;
+
+public class productItem {
+
+        private int id;
+        private String productName;
+        private double cost;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getProductName() {
+            return productName;
+        }
+
+        public void setProductName(String productName) {
+            this.productName = productName;
+        }
+
+        public double getCost() {
+            return cost;
+        }
+
+        public void setCost(double cost) {
+            this.cost = cost;
+        }
+}
--- src/main/java/ViewModel/Main.java
+++ src/main/java/ViewModel/Main.java
@@ -1,4 +1,4 @@
-package eng;
+package ViewModel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
